fb7e224103c0cf87c42a7110e515b87050163f20
leonhiat <leonhiat33@gmail.com>
2023-11-03 16:40:04 -0700

    This is the fourth landing page.

--- src/components/layout/landing.tsx
+++ src/components/layout/landing.tsx
@@ -96,6 +96,26 @@ const Landing: FC = () => {
           </div>
         </WhyContainer>
       </div>
+      <div className="about">
+        <div className="about_container">
+          <div className="text_container">
+            <div className="text_container_header">About Us</div>
+            <div className="text_container_text1">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </div>
+            <div className="text_container_text2">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </div>
+          </div>
+          <img src={"/assets/img/about.png"} alt="This is the about png." className="png_about" />
+        </div>
+      </div>
     </div>
   );
 };
@@ -235,4 +255,4 @@ const WhyHeader = styled.div`
   font-weight: 700;
   word-wrap: break-word;
   text-align: center;
-`
+`;
