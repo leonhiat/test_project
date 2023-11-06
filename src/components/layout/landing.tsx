d10c7634e2e2af1375bbc6c86099e81854e6a035
leonhiat <leonhiat33@gmail.com>
2023-11-05 18:05:32 -0800

    This is the responsive fifth landing page.

--- src/components/layout/landing.tsx
+++ src/components/layout/landing.tsx
@@ -11,13 +11,11 @@ const Landing: FC = () => {
           <Header />
         </div>
         <div className="home_text">
-          <div className="big_home_text">
-            Phoniex Security Service
-          </div>
+          <div className="big_home_text">Phoniex Security Service</div>
           <div className="small_home_text">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text ever
-            since the 1500s, when an
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an
           </div>
           <div className="home_button">
             <p className="button_text">Get Started</p>
@@ -96,7 +94,7 @@ const Landing: FC = () => {
         </div>
       </div>
 
-      {/* <div className="about" id="about">
+      <div className="about" id="about">
         <div className="about_container">
           <div className="text_container">
             <div className="text_container_header">About Us</div>
@@ -120,225 +118,64 @@ const Landing: FC = () => {
           />
         </div>
       </div>
+
       <div className="clients">
-        <div className="clients_header">What clients say</div>
-        <div className="client1">
-          <img
-            src={"/assets/img/client1.png"}
-            alt="This is the client1 png."
-            className="png_client1"
-          />
-          <div className="stars">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 53 53"
-              fill="none"
-            >
-              <path
-                d="M34.3653 18.5083L29.4653 2.375C28.4987 -0.791667 24.032 -0.791667 23.0987 2.375L18.1653 18.5083H3.33199C0.0986578 18.5083 -1.23468 22.675 1.39866 24.5417L13.532 33.2083L8.76532 48.575C7.79866 51.675 11.3987 54.175 13.9653 52.2083L26.2653 42.875L38.5653 52.2417C41.132 54.2083 44.732 51.7083 43.7653 48.6083L38.9987 33.2417L51.132 24.575C53.7653 22.675 52.432 18.5417 49.1987 18.5417H34.3653V18.5083Z"
-                fill="#FFFF00"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 53 53"
-              fill="none"
-            >
-              <path
-                d="M34.3653 18.5083L29.4653 2.375C28.4987 -0.791667 24.032 -0.791667 23.0987 2.375L18.1653 18.5083H3.33199C0.0986578 18.5083 -1.23468 22.675 1.39866 24.5417L13.532 33.2083L8.76532 48.575C7.79866 51.675 11.3987 54.175 13.9653 52.2083L26.2653 42.875L38.5653 52.2417C41.132 54.2083 44.732 51.7083 43.7653 48.6083L38.9987 33.2417L51.132 24.575C53.7653 22.675 52.432 18.5417 49.1987 18.5417H34.3653V18.5083Z"
-                fill="#FFFF00"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 53 53"
-              fill="none"
-            >
-              <path
-                d="M34.3653 18.5083L29.4653 2.375C28.4987 -0.791667 24.032 -0.791667 23.0987 2.375L18.1653 18.5083H3.33199C0.0986578 18.5083 -1.23468 22.675 1.39866 24.5417L13.532 33.2083L8.76532 48.575C7.79866 51.675 11.3987 54.175 13.9653 52.2083L26.2653 42.875L38.5653 52.2417C41.132 54.2083 44.732 51.7083 43.7653 48.6083L38.9987 33.2417L51.132 24.575C53.7653 22.675 52.432 18.5417 49.1987 18.5417H34.3653V18.5083Z"
-                fill="#FFFF00"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 53 53"
-              fill="none"
-            >
-              <path
-                d="M34.3653 18.5083L29.4653 2.375C28.4987 -0.791667 24.032 -0.791667 23.0987 2.375L18.1653 18.5083H3.33199C0.0986578 18.5083 -1.23468 22.675 1.39866 24.5417L13.532 33.2083L8.76532 48.575C7.79866 51.675 11.3987 54.175 13.9653 52.2083L26.2653 42.875L38.5653 52.2417C41.132 54.2083 44.732 51.7083 43.7653 48.6083L38.9987 33.2417L51.132 24.575C53.7653 22.675 52.432 18.5417 49.1987 18.5417H34.3653V18.5083Z"
-                fill="#FFFF00"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 53 53"
-              fill="none"
-            >
-              <path
-                d="M34.3653 18.5083L29.4653 2.375C28.4987 -0.791667 24.032 -0.791667 23.0987 2.375L18.1653 18.5083H3.33199C0.0986578 18.5083 -1.23468 22.675 1.39866 24.5417L13.532 33.2083L8.76532 48.575C7.79866 51.675 11.3987 54.175 13.9653 52.2083L26.2653 42.875L38.5653 52.2417C41.132 54.2083 44.732 51.7083 43.7653 48.6083L38.9987 33.2417L51.132 24.575C53.7653 22.675 52.432 18.5417 49.1987 18.5417H34.3653V18.5083Z"
-                fill="#FFFF00"
-              />
-            </svg>
+        <div className="clients_head">What clients say</div>
+        <div className="clients_container">
+          <div className="client1">
+            <img
+              src={"/assets/img/client1.png"}
+              alt="this is the client1 png."
+              className="bothPng"
+            />
+            <div className="star_container">
+              <img src={"/assets/img/star.png"} alt="This is the star png." />
+              <img src={"/assets/img/star.png"} alt="This is the star png." />
+              <img src={"/assets/img/star.png"} alt="This is the star png." />
+              <img src={"/assets/img/star.png"} alt="This is the star png." />
+              <img src={"/assets/img/star.png"} alt="This is the star png." />
+            </div>
           </div>
-        </div>
-        <div className="client2">
-          <img
-            src={"/assets/img/client2.png"}
-            alt="This is the client2 png."
-            className="png_client2"
-          />
-          <div className="stars">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 53 53"
-              fill="none"
-            >
-              <path
-                d="M34.3653 18.5083L29.4653 2.375C28.4987 -0.791667 24.032 -0.791667 23.0987 2.375L18.1653 18.5083H3.33199C0.0986578 18.5083 -1.23468 22.675 1.39866 24.5417L13.532 33.2083L8.76532 48.575C7.79866 51.675 11.3987 54.175 13.9653 52.2083L26.2653 42.875L38.5653 52.2417C41.132 54.2083 44.732 51.7083 43.7653 48.6083L38.9987 33.2417L51.132 24.575C53.7653 22.675 52.432 18.5417 49.1987 18.5417H34.3653V18.5083Z"
-                fill="#FFFF00"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 53 53"
-              fill="none"
-            >
-              <path
-                d="M34.3653 18.5083L29.4653 2.375C28.4987 -0.791667 24.032 -0.791667 23.0987 2.375L18.1653 18.5083H3.33199C0.0986578 18.5083 -1.23468 22.675 1.39866 24.5417L13.532 33.2083L8.76532 48.575C7.79866 51.675 11.3987 54.175 13.9653 52.2083L26.2653 42.875L38.5653 52.2417C41.132 54.2083 44.732 51.7083 43.7653 48.6083L38.9987 33.2417L51.132 24.575C53.7653 22.675 52.432 18.5417 49.1987 18.5417H34.3653V18.5083Z"
-                fill="#FFFF00"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 53 53"
-              fill="none"
-            >
-              <path
-                d="M34.3653 18.5083L29.4653 2.375C28.4987 -0.791667 24.032 -0.791667 23.0987 2.375L18.1653 18.5083H3.33199C0.0986578 18.5083 -1.23468 22.675 1.39866 24.5417L13.532 33.2083L8.76532 48.575C7.79866 51.675 11.3987 54.175 13.9653 52.2083L26.2653 42.875L38.5653 52.2417C41.132 54.2083 44.732 51.7083 43.7653 48.6083L38.9987 33.2417L51.132 24.575C53.7653 22.675 52.432 18.5417 49.1987 18.5417H34.3653V18.5083Z"
-                fill="#FFFF00"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 53 53"
-              fill="none"
-            >
-              <path
-                d="M34.3653 18.5083L29.4653 2.375C28.4987 -0.791667 24.032 -0.791667 23.0987 2.375L18.1653 18.5083H3.33199C0.0986578 18.5083 -1.23468 22.675 1.39866 24.5417L13.532 33.2083L8.76532 48.575C7.79866 51.675 11.3987 54.175 13.9653 52.2083L26.2653 42.875L38.5653 52.2417C41.132 54.2083 44.732 51.7083 43.7653 48.6083L38.9987 33.2417L51.132 24.575C53.7653 22.675 52.432 18.5417 49.1987 18.5417H34.3653V18.5083Z"
-                fill="#FFFF00"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 53 53"
-              fill="none"
-            >
-              <path
-                d="M34.3653 18.5083L29.4653 2.375C28.4987 -0.791667 24.032 -0.791667 23.0987 2.375L18.1653 18.5083H3.33199C0.0986578 18.5083 -1.23468 22.675 1.39866 24.5417L13.532 33.2083L8.76532 48.575C7.79866 51.675 11.3987 54.175 13.9653 52.2083L26.2653 42.875L38.5653 52.2417C41.132 54.2083 44.732 51.7083 43.7653 48.6083L38.9987 33.2417L51.132 24.575C53.7653 22.675 52.432 18.5417 49.1987 18.5417H34.3653V18.5083Z"
-                fill="#FFFF00"
-              />
-            </svg>
+          <div className="client2">
+            <img
+              src={"/assets/img/client2.png"}
+              alt="this is the client1 png."
+              className="png_client2"
+            />
+            <div className="star_container">
+              <img src={"/assets/img/star.png"} alt="This is the star png." />
+              <img src={"/assets/img/star.png"} alt="This is the star png." />
+              <img src={"/assets/img/star.png"} alt="This is the star png." />
+              <img src={"/assets/img/star.png"} alt="This is the star png." />
+              <img src={"/assets/img/star.png"} alt="This is the star png." />
+            </div>
+            <div className="client_text">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </div>
           </div>
-        </div>
-        <div className="client3">
-          <img
-            src={"/assets/img/client3.png"}
-            alt="This is the client3 png."
-            className="png_client3"
-          />
-          <div className="stars">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              viewBox="0 0 53 53"
-              fill="none"
-            >
-              <path
-                d="M34.3653 18.5083L29.4653 2.375C28.4987 -0.791667 24.032 -0.791667 23.0987 2.375L18.1653 18.5083H3.33199C0.0986578 18.5083 -1.23468 22.675 1.39866 24.5417L13.532 33.2083L8.76532 48.575C7.79866 51.675 11.3987 54.175 13.9653 52.2083L26.2653 42.875L38.5653 52.2417C41.132 54.2083 44.732 51.7083 43.7653 48.6083L38.9987 33.2417L51.132 24.575C53.7653 22.675 52.432 18.5417 49.1987 18.5417H34.3653V18.5083Z"
-                fill="#FFFF00"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              viewBox="0 0 53 53"
-              fill="none"
-            >
-              <path
-                d="M34.3653 18.5083L29.4653 2.375C28.4987 -0.791667 24.032 -0.791667 23.0987 2.375L18.1653 18.5083H3.33199C0.0986578 18.5083 -1.23468 22.675 1.39866 24.5417L13.532 33.2083L8.76532 48.575C7.79866 51.675 11.3987 54.175 13.9653 52.2083L26.2653 42.875L38.5653 52.2417C41.132 54.2083 44.732 51.7083 43.7653 48.6083L38.9987 33.2417L51.132 24.575C53.7653 22.675 52.432 18.5417 49.1987 18.5417H34.3653V18.5083Z"
-                fill="#FFFF00"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              viewBox="0 0 53 53"
-              fill="none"
-            >
-              <path
-                d="M34.3653 18.5083L29.4653 2.375C28.4987 -0.791667 24.032 -0.791667 23.0987 2.375L18.1653 18.5083H3.33199C0.0986578 18.5083 -1.23468 22.675 1.39866 24.5417L13.532 33.2083L8.76532 48.575C7.79866 51.675 11.3987 54.175 13.9653 52.2083L26.2653 42.875L38.5653 52.2417C41.132 54.2083 44.732 51.7083 43.7653 48.6083L38.9987 33.2417L51.132 24.575C53.7653 22.675 52.432 18.5417 49.1987 18.5417H34.3653V18.5083Z"
-                fill="#FFFF00"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              viewBox="0 0 53 53"
-              fill="none"
-            >
-              <path
-                d="M34.3653 18.5083L29.4653 2.375C28.4987 -0.791667 24.032 -0.791667 23.0987 2.375L18.1653 18.5083H3.33199C0.0986578 18.5083 -1.23468 22.675 1.39866 24.5417L13.532 33.2083L8.76532 48.575C7.79866 51.675 11.3987 54.175 13.9653 52.2083L26.2653 42.875L38.5653 52.2417C41.132 54.2083 44.732 51.7083 43.7653 48.6083L38.9987 33.2417L51.132 24.575C53.7653 22.675 52.432 18.5417 49.1987 18.5417H34.3653V18.5083Z"
-                fill="#FFFF00"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              viewBox="0 0 53 53"
-              fill="none"
-            >
-              <path
-                d="M34.3653 18.5083L29.4653 2.375C28.4987 -0.791667 24.032 -0.791667 23.0987 2.375L18.1653 18.5083H3.33199C0.0986578 18.5083 -1.23468 22.675 1.39866 24.5417L13.532 33.2083L8.76532 48.575C7.79866 51.675 11.3987 54.175 13.9653 52.2083L26.2653 42.875L38.5653 52.2417C41.132 54.2083 44.732 51.7083 43.7653 48.6083L38.9987 33.2417L51.132 24.575C53.7653 22.675 52.432 18.5417 49.1987 18.5417H34.3653V18.5083Z"
-                fill="#FFFF00"
-              />
-            </svg>
-          </div>
-          <div className="client_say">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+          <div className="client3">
+            <img
+              src={"/assets/img/client3.png"}
+              alt="this is the client3 png."
+              className="bothPng"
+            />
+            <div className="star_container">
+              <img src={"/assets/img/star.png"} alt="This is the star png." />
+              <img src={"/assets/img/star.png"} alt="This is the star png." />
+              <img src={"/assets/img/star.png"} alt="This is the star png." />
+              <img src={"/assets/img/star.png"} alt="This is the star png." />
+              <img src={"/assets/img/star.png"} alt="This is the star png." />
+            </div>
           </div>
         </div>
       </div>
+
       <div className="contact" id="contact">
+        <div className="contact_text_header">Contact Us</div>
         <div className="contact_container">
-          <div className="contact_text_header">Contact Us</div>
           <div className="contact_text_container">
             <div className="contact_group1">
               <svg
@@ -428,7 +265,7 @@ const Landing: FC = () => {
           </div>
         </div>
       </div>
-      <div className="footer">
+      {/* <div className="footer">
         <div className="cover_footer">
           <img
             src={"/assets/img/footer_png.png"}
