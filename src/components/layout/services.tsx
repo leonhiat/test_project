046f54c878de551e0fe75e080e8274f752b07664
leonhiat <leonhiat33@gmail.com>
2023-11-07 12:06:11 -0800

    This is the completed responsive second.

--- src/components/layout/services.tsx
+++ src/components/layout/services.tsx
@@ -3,38 +3,39 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { styled } from "@mui/system"
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#fff",
+    palette: {
+        primary: {
+            main: "#fff",
+        },
     },
-  },
 });
 
 const Services = () => {
     return (
         <div>
-            <Box className="Secondpart" sx={{ marginTop: "278px" }}>
+            <SecondPart>
                 <Box sx={{ flexGrow: 1 }}>
                     <Grid container px={2} spacing={12}>
                         <Grid item xs={12} lg={6}>
-                            <img />
+                            <img alt='this is the security png.' className='png_second' />
                         </Grid>
                         <Grid item xs={12} lg={6}>
-                            <h2>Our Services</h2>
-                            <p>
+                            <SecondPartHead>Our Services</SecondPartHead>
+                            <SecondPartText1>
                                 Ipsum is simply dummy text of the printing and typesetting
                                 industry. Lorem Ipsum has been the industry's standard dummy
                                 text ever since the 1500s, when an unknown printer took a galley
                                 of type and scrambled it to make a type specimen book.
-                            </p>
-                            <p>
+                            </SecondPartText1>
+                            <SecondPartText2>
                                 Lorem Ipsum is simply dummy text of the printing and typesetting
                                 industry. Lorem Ipsum has been the industry's standard dummy
                                 text ever since the 1500s, when an unknown printer took a galley
                                 of type and scrambled it to make a type specimen book.
-                            </p>
+                            </SecondPartText2>
                             <ThemeProvider theme={theme}>
                                 <Button
                                     variant="outlined"
@@ -50,9 +51,71 @@ const Services = () => {
                         </Grid>
                     </Grid>
                 </Box>
-            </Box>
+            </SecondPart>
         </div>
     )
 }
 
 export default Services;
+
+const SecondPart = styled(Box)(({ theme }) => ({
+    marginTop: '260px',
+    [theme.breakpoints.up(600)]:{
+        width: '75%',
+        margin: '0 auto',
+        marginTop: '260px',
+    },
+    [theme.breakpoints.up(992)]:{
+        width: '66%',
+        margin: '0 auto',
+        marginTop: '260px',
+    },
+    [theme.breakpoints.up(1200)]:{
+        width: '96%',
+        margin: '0 24px',
+        marginTop: '260px',
+    },
+}));
+
+const SecondPartHead = styled(Box)(({ theme }) => ({
+    fontFamily: 'Montserrat',
+    fontSize: '24px',
+    fontWeight: '700',
+    [theme.breakpoints.up(600)]:{
+        fontSize: '24px'
+    },
+    [theme.breakpoints.up(768)]:{
+        fontSize: '32px'
+    },
+    [theme.breakpoints.up(1200)]:{
+        fontSize: '50px'
+    },
+}))
+
+const SecondPartText1 = styled(Box)(({ theme }) => ({
+    fontFamily: 'Poppins',
+    fontSize: '12px',
+    fontWeight: '500',
+    lineHeight: '36px',
+    margin: '30px 0',
+    [theme.breakpoints.up(600)]:{
+        fontSize: '16px'
+    },
+    [theme.breakpoints.up(768)]:{
+        fontSize: '18px'
+    },
+}))
+
+const SecondPartText2 = styled(Box)(({ theme }) => ({
+    fontFamily: 'Poppins',
+    fontSize: '12px',
+    fontWeight: '500',
+    lineHeight: '36px',
+    margin: '30px 0',
+    [theme.breakpoints.up(600)]:{
+        fontSize: '16px'
+    },
+    [theme.breakpoints.up(768)]:{
+        fontSize: '18px'
+    },
+}))
